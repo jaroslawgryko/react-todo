7707cdbf5e4a83b683657717fe53a45bb208da5c
jaroslaw gryko <jaroslaw.gryko@gmail.com>
2017-01-29 00:13:28 +0100

    Create AddTodo component

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,40 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require ('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+    it('should exist', () => {
+        expect(AddTodo).toExist();
+    });
+
+    it('should call onAddTodo prop with valid data', () => {
+        var todoTextToCheck = 'Check mail';
+
+        var spy = expect.createSpy();
+        var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(addTodo));
+
+        addTodo.refs.todoText.value = todoTextToCheck;
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toHaveBeenCalledWith(todoTextToCheck);
+    });
+
+    it('should not call onAddTodo prop when invalid input', () => {
+        var todoTextToCheck = '';
+
+        var spy = expect.createSpy();
+        var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(addTodo));
+
+        addTodo.refs.todoText.value = todoTextToCheck;
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toNotHaveBeenCalled();
+    });
+
+});
